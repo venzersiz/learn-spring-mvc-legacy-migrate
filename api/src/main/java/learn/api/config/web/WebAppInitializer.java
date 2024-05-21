6b5e72f601a886006615cc9143e7f28ba917b56f
Krevis <venzersiz@gmail.com>
2024-05-21 13:33:59 +0900

    Java Config > Java Config + XML Config

--- api/src/main/java/learn/api/config/web/WebAppInitializer.java
+++ api/src/main/java/learn/api/config/web/WebAppInitializer.java
@@ -1,12 +1,10 @@
 package learn.api.config.web;
 
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
-import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class WebAppInitializer implements WebApplicationInitializer {
@@ -14,16 +12,14 @@ public class WebAppInitializer implements WebApplicationInitializer {
     @Override
     public void onStartup(ServletContext servletContext) {
 
-        XmlWebApplicationContext rootAppContext = new XmlWebApplicationContext();
-        rootAppContext.setConfigLocation("/WEB-INF/spring/applicationContext.xml");
+        // ~ Root application context
+        AnnotationConfigWebApplicationContext appContext = new AnnotationConfigWebApplicationContext();
+        appContext.setConfigLocation("learn.api.config");
 
-        servletContext.addListener(new ContextLoaderListener(rootAppContext));
+        servletContext.addListener(new ContextLoaderListener(appContext));
 
-
-        XmlWebApplicationContext servletAppContext = new XmlWebApplicationContext();
-        servletAppContext.setConfigLocation("/WEB-INF/spring/servletContext.xml");
-
-        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcherServlet", new DispatcherServlet(servletAppContext));
+        // ~ Servlet application context
+        ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcherServlet", new DispatcherServlet(appContext));
 
         dispatcher.addMapping("/");
         dispatcher.setLoadOnStartup(1);
