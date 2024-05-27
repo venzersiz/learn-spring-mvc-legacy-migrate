a5a67e2abf2b28998abcc3bf4cdf33acfc11c192
Krevis <venzersiz@gmail.com>
2024-05-27 14:53:14 +0900

    Spring Boot > DataSource

--- admin/src/main/java/learn/api/config/spring/PersistenceConfig.java
+++ admin/src/main/java/learn/api/config/spring/PersistenceConfig.java
@@ -2,12 +2,10 @@ package learn.api.config.spring;
 
 import javax.sql.DataSource;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,28 +17,6 @@ import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 public class PersistenceConfig {
 
     private final ApplicationContext applicationContext;
-
-    @Value("${db.driverClassName}")
-    private String driverClassName;
-
-    @Value("${db.url}")
-    private String url;
-
-    @Value("${db.username}")
-    private String username;
-
-    @Value("${db.password}")
-    private String password;
-
-    @Bean
-    public DataSource dataSource() {
-        BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setDriverClassName(driverClassName);
-        dataSource.setUrl(url);
-        dataSource.setUsername(username);
-        dataSource.setPassword(password);
-        return dataSource;
-    }
 
     @Bean
     public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
